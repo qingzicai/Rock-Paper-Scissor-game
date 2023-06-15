70767dd256c7c97a3de2ff33371322ef275eb338
ivy <dllmwl08@gmail.com>
2023-06-14 23:30:31 -0400

    need to do reset

--- app.js
+++ app.js
@@ -4,9 +4,8 @@
 // //compare botPick to userPick: const = possibleResult{(userPic = "scissor" && botPic = "scissor"): "it's a tie"...etc.}; 
 // //return "You beat the Bot!" or return "How can you loose to the bot? Try again!" or "It's a tie!" in the <div id="resultDisplay"></div> area;
 
-
-
 const userChoice = document.querySelectorAll("li");
+
 const possibleResults = {
   rock: {
     rock: "It's a tie!",
@@ -32,37 +31,12 @@ function getRandomChoice() {
 }
 
 function displayResult(result) {
-  const resultDisplay = document.getElementById("resultDisplay");
-  resultDisplay.textContent = result;
-
-  const resetContainer = document.getElementById("reset");
-  resetContainer.innerHTML = ""; // Clear any previous content
-
-  const resetButton = document.createElement("button");
-  resetButton.textContent = "Do it again";
-  resetButton.addEventListener("click", resetGame);
-  resetContainer.appendChild(resetButton);
-}
-
-function resetGame() {
-  const choiceContainer = document.getElementById("choice");
-  choiceContainer.innerHTML = `
-    <li id="rock"><img src="rock.png" alt="rock" width="400" height="400"></li>
-    <li id="scissor"><img src="scissor.png" alt="scissor" width="400" height="400"></li>
-    <li id="paper"><img src="paper.png" alt="paper" width="400" height="400"></li>
-  `;
-
-  const resetContainer = document.getElementById("reset");
-  resetContainer.innerHTML = ""; // Clear the reset button
-
-  userChoice.forEach((choice) => {
-    choice.addEventListener("click", () => {
-      const userPick = choice.id;
-      const botPick = getRandomChoice();
-      const result = possibleResults[userPick][botPick];
-      displayResult(result);
-    });
-  });
+    const resultDisplay = document.getElementById("resultDisplay");
+    resultDisplay.innerHTML = `
+    <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
+    <p style="font-weight: bold; font-size: 24px;">${result}</p>
+    </div>
+    `;
 }
 
 userChoice.forEach((choice) => {
@@ -70,8 +44,80 @@ userChoice.forEach((choice) => {
     const userPick = choice.id;
     const botPick = getRandomChoice();
     const result = possibleResults[userPick][botPick];
+    document.getElementById('choice').innerHTML = "";
     displayResult(result);
   });
 });
+
+
+// const userChoice = document.querySelectorAll("li");
+
+// const possibleResults = {
+//     rock: {
+//         rock: "It's a tie!",
+//         paper: "How can you lose to the bot? Try again!",
+//         scissor: "You beat the bot!"
+//     },
+//     paper: {
+//         rock: "You beat the bot!",
+//         paper: "It's a tie!",
+//         scissor: "How can you lose to the bot? Try again!"
+//     },
+//     scissor: {
+//         rock: "How can you lose to the bot? Try again!",
+//         paper: "You beat the bot!",
+//         scissor: "It's a tie!"
+//     }
+// };
+
+// function getRandomChoice() {
+//     const choices = ["rock", "paper", "scissor"];
+//     const randomChoice = choices.random();
+//     return randomChoice;
+// }
+
+// function displayResult(result) {
+//     const resultDisplay = document.getElementById("resultDisplay");
+//     resultDisplay.textContent = result;
+
+//     const resetContainer = document.getElementById("reset");
+//     resetContainer.innerHTML = ""; 
+
+//     const resetButton = document.createElement("button");
+//     resetButton.textContent = "Do it again";
+//     resetButton.addEventListener("click", resetGame);
+//     resetContainer.appendChild(resetButton);
+//     }
+
+// function resetGame() {
+//     const choiceContainer = document.getElementById("choice");
+//     choiceContainer.innerHTML = `
+//         <li id="rock"><img src="rock.png" alt="rock" width="400" height="400"></li>
+//         <li id="scissor"><img src="scissor.png" alt="scissor" width="400" height="400"></li>
+//         <li id="paper"><img src="paper.png" alt="paper" width="400" height="400"></li>
+//     `;
+
+//     const resetContainer = document.getElementById("reset");
+//     resetContainer.innerHTML = "";
+
+//     userChoice.forEach((choice) => {
+//         choice.addEventListener("click", () => {
+//         const userPick = choice.id;
+//         const botPick = getRandomChoice();
+//         const result = possibleResults[userPick][botPick];
+//         displayResult(result);
+//         });
+//     });
+// }
+
+// userChoice.forEach((choice) => {
+//     choice.addEventListener("click", () => {
+//         const userPick = choice.id;
+//         const botPick = getRandomChoice();
+//         const result = possibleResults[userPick][botPick];
+           
+//         displayResult(result);
+//     });
+// });
 
 
